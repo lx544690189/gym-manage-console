f8976a28c2abc5b579e606719700d91ba36ba84d
李鑫 <lixin@cai-inc.com>
2019-02-20 15:42:01 +0800

    [modify] - 优化代码结构； - 增加启动任务；

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const LocalStrategy = require('passport-local').Strategy;
+class GymApp {
+  constructor(app) {
+    this.app = app;
+  }
+  /**
+   * @description
+    - 所有的插件都已启动完毕，但是应用整体还未 ready
+    - 可以做一些数据初始化等操作，这些操作成功才会启动应用
+   * @memberof GymApp
+   */
+  async willReady() {
+    initPasspot(this.app);
+    initQiniuTask(this.app);
+  }
+}
 
-module.exports = app => {
-  // 挂载 strategy
+module.exports = GymApp;
+
+/**
+ * @description 初始化passport插件
+ * @param {*} app
+ */
+function initPasspot(app) {
   app.passport.use(new LocalStrategy({
     passReqToCallback: true,
   }, (req, username, password, done) => {
@@ -22,11 +42,17 @@ module.exports = app => {
     }
   });
   app.passport.serializeUser(async (ctx, user) => {
-    // console.log('user: ', user);
     return user;
   });
   app.passport.deserializeUser(async (ctx, user) => {
-    // console.log('user: ', user);
     return user;
   });
-};
+}
+
+/**
+ * @description 七牛云获取token定时任务，需要在应用启动时手动执行一次
+ * @param {*} app
+ */
+async function initQiniuTask(app) {
+  await app.runSchedule('qiniu');
+}
