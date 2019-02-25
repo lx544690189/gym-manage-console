87320ca4666cb2acaa989bd813d6d45687098f77
lx544690189 <lixin544@163.com>
2019-02-25 15:30:29 +0800

    [modify]移除登录处理逻辑

--- app.js
+++ app.js
@@ -22,28 +22,11 @@ module.exports = GymApp;
  * @param {*} app
  */
 function initPasspot(app) {
-  app.passport.use(new LocalStrategy({
-    passReqToCallback: true,
-  }, (req, username, password, done) => {
-    // format user
-    const user = {
-      provider: 'local',
-      username,
-      password,
-    };
-    app.passport.doVerify(req, user, done);
-  }));
-
-  // 处理用户信息
-  app.passport.verify(async (ctx, { username, password }) => {
-    const response = await ctx.service.accounts.login({ username, password });
-    if (response.success) {
-      return response.data;
-    }
-  });
+  // 序列化用户信息到session
   app.passport.serializeUser(async (ctx, user) => {
     return user;
   });
+  // 反序列化用户信息
   app.passport.deserializeUser(async (ctx, user) => {
     return user;
   });
